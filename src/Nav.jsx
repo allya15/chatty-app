113cbe3a76244198ed84711cb1fc7146292c92dd
Ally <allison.avis@gmail.com>
2019-01-14 10:32:05 +0000

    styling changes

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 
 function Nav(props){
-
-  return (
-    <nav className="navbar">
-      <a href="/" className="navbar-brand">Chatty</a>
-      <p className="user-count">Users Online: {props.usersOnline}</p>
-    </nav>
-  );
-}
+        return (
+            <nav className="navbar">
+                <a href="/" className="navbar-brand">Trailer Park Talk</a>
+                <p className="user-count">residents in the park{props.usersOnline > 1 ? ': ' + props.usersOnline : ': just you mate'}</p>
+            </nav>
+        )
+    }
 
 export default Nav;
